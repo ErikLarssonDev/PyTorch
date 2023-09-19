92f758f4645ee239cb2c614062221a7c92aaeec2
Erik Larsson <erik.larsson@weknowit.se>
2023-09-19 16:53:49 +0200

    Created train.py

---
+++ U-NET_image_segmentation/train.py
@@ -0,0 +1,107 @@
+import torch 
+import albumentations as A 
+from albumentations.pytorch import ToTensorV2 
+from tqdm import tqdm 
+import torch.nn as nn 
+import torch.optim as optim 
+from model import UNET 
+# from utils import (
+#     load_checkpoint,
+#     save_checkpoint,
+#     get_loaders,
+#     check_accuracy,
+#     save_predictions_as_img
+# )
+
+# Hyperparameters etc.
+LEARNING_RATE = 1e-4
+DEVICE = "mps" if torch.backends.mps.is_available() else ("cuda" if torch.cuda.is_available() else "cpu")
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
+NUM_WORKERS = 0 # os.cpu_count()
+IMAGE_HEIGHT = 160 # Originally 1280
+IMAGE_WIDTH = 240 # Originally 1918
+PIN_MEMORY = True
+LOAD_MODEL = False
+DATASET_DIR = "../carvana-image-masking-challenge"
+TRAIN_IMG_DIR = DATASET_DIR + "/train_images/"
+TRAIN_MASK_DIR = DATASET_DIR + "/train_masks/"
+VAL_IMG_DIR = DATASET_DIR + "/val_images/"
+VAL_MASK_DIR = DATASET_DIR + "/val_masks/"
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    loop = tqdm(loader)
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(DEVICE)
+        targets = targets.float().unsqueeze(1).to(DEVICE)
+
+        # forward
+        with torch.cuda.amp.autocast():
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+        
+        # backward
+        optimizer.zero_grad()
+        scaler,scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # Update tqdm loop
+        loop.set_postfix(loss=loss.item())
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    ) 
+
+    val_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+             A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transform,
+        NUM_WORKERS,
+        PIN_MEMORY,
+    )
+
+    scaler = torch.cuda.amp.GradScaler()
+
+    for epoch in range(NUM_EPOCHS):
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+        # Save model 
+        # Check accuracy
+        # Print some examples to a folder
+
+
+if __name__ == "__main__":
+    main() # Test and see if this solves the num_workers bug
