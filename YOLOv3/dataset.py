92b7f5418337376561e54b0ee4b0a695bc746f9b
Erik Larsson <erik.larsson@weknowit.se>
2023-09-22 12:56:32 +0200

    Simple logistic regression for the titanic challenge as a baseline.

--- YOLOv3/dataset.py
+++ YOLOv3/dataset.py
@@ -45,44 +45,6 @@ class YOLODataset(Dataset):
     def __len__(self):
         return len(self.annotations)
     
-    # def __getitem__(self, index):
-    #     label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
-    #     bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2), 4, axis=1).tolist() # Using np.roll to get classes at the end
-    #     img_path = os.path.join(self.img_dir, self.annotations.iloc[index, 0])
-    #     image = np.array(Image.open(img_path).convert("RGB")) # Make sure it is in RGB, think there is greyscale in COCO
-
-    #     if self.transform:
-    #         augmentations = self.transform(image=image, bboxes=bboxes)
-    #         image = augmentations["image"]
-    #         bboxes = augmentations["bboxes"]
-        
-    #     targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S] # [p_o, x, y, w, h, c] 
-        
-    #     for box in bboxes:
-    #         iou_anchors = iou(torch.tensor(box[2:4]), self.anchors)
-    #         anchor_indices = iou_anchors.argsort(descending=True, dim=0)
-    #         x, y, width, height, class_label = box 
-    #         has_anchor = [False, False, False]
-
-    #         for anchor_idx in anchor_indices:
-    #             scale_idx = anchor_idx // self.num_anchors_per_scale # 0, 1, 2, which scale
-    #             anchor_on_scale = anchor_idx & self.num_anchors_per_scale # 0, 1, 2, which anchor
-    #             S = self.S[scale_idx]
-    #             i, j = int(S*y), int(S*x) # x=0.5, S=13 --> int(6.5) = 6
-    #             anchor_taken = targets[scale_idx][anchor_on_scale, i, j, 0]
-    #             if not anchor_taken and not has_anchor[scale_idx]:
-    #                 targets[scale_idx][anchor_on_scale, i, j, 0] = 1
-    #                 x_cell, y_cell = S*x - j, S*y - i # both are between [0, 1]
-    #                 width_cell, height_cell = width*S, height*S 
-    #                 box_coordinates = torch.tensor([x_cell, y_cell, width_cell, height_cell])
-    #                 targets[scale_idx][anchor_on_scale, i, j, 1:5] = box_coordinates
-    #                 targets[scale_idx][anchor_on_scale, i, j, 5] = int(class_label)
-    #                 has_anchor[scale_idx] = True
-                    
-    #             elif not anchor_taken and iou_anchors[anchor_idx] > self.ignore_iou_thresh:
-    #                 targets[scale_idx][anchor_on_scale, i, j, 0] = -1 # ignore this prediction 
-                    
-    #     return image, tuple(targets)
     def __getitem__(self, index):
         label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
         bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2), 4, axis=1).tolist()
