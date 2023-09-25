f55dc91c309f370f31ee5bab8bd4eb3e869012d4
Erik Larsson <erik.larsson@weknowit.se>
2023-09-25 16:42:23 +0200

    Added docstring

--- object_detection/iou.py
+++ object_detection/iou.py
@@ -1,6 +1,17 @@
 import torch 
 
 def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
+    """
+    Calculates intersection over union
+
+    Parameters:
+        boxes_preds (tensor): Predictions of Bounding Boxes (BATCH_SIZE, 4)
+        boxes_labels (tensor): Correct Labels of Boxes (BATCH_SIZE, 4)
+        box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
+
+    Returns:
+        tensor: Intersection over union for all examples
+    """
     # boxes_preds shape is (N, 4) where N is the number of bboxes
     if box_format == "midpoint":
         box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
