7ae95997367b14e592551850847322c713860a60
Erik Larsson <erik.larsson@weknowit.se>
2023-09-25 12:34:48 +0200

    Baseline solution and extended solution with 1 epoch of fine tuning.

---
+++ kaggle_dogs_vs_cats_redux_kernels_edition/train.py
@@ -0,0 +1,98 @@
+import os
+import torch 
+import torch.nn.functional as F 
+import numpy as np 
+import config 
+from torch import nn, optim 
+from torch.utils.data import DataLoader 
+from tqdm import tqdm 
+from dataset import CatDog 
+from efficientnet_pytorch import EfficientNet
+from utils import check_accuracy, load_checkpoint, save_checkpoint
+
+# Idea: Use Efficientnet_b7 to extract features and then train a small model on those features
+# Hopefully fast training with good performance
+def save_feature_vectors(model, loader, output_size=(1, 1), file="trainb7"):
+    model.eval()
+    images, labels = [], []
+
+    for idx, (x, y) in enumerate(tqdm(loader)):
+        x = x.to(config.DEVICE)
+
+        with torch.no_grad():
+            features = model.extract_features(x)
+            features = F.adaptive_avg_pool2d(features, output_size=output_size)
+        images.append(features.reshape(x.shape[0], -1).detach().cpu().numpy())
+        labels.append(y.numpy())
+        
+    np.save(f"data_features/X_{file}.npy", np.concatenate(images, axis=0))
+    np.save(f"data_features/y_{file}.npy", np.concatenate(labels, axis=0))
+    model.train()
+    
+
+def train_one_epoch(loader, model, loss_fn, optimizer, scaler):
+    loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(config.DEVICE)
+        targets = targets.to(config.DEVICE)
+        
+        with torch.cuda.amp.autocast():
+            scores = model(data)
+            loss = loss_fn(scores, targets)
+        
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        loop.set_postfix(loss=loss.item())
+
+
+def main():
+    model = EfficientNet.from_pretrained("efficientnet-b7")
+    model._fc = nn.Linear(2560, 1)
+    train_dataset = CatDog(root="../dogs-vs-cats-redux-kernels-edition/train/", transform=config.basic_transform)
+    test_dataset = CatDog(root="../dogs-vs-cats-redux-kernels-edition/test/", transform=config.basic_transform)
+    train_loader = DataLoader(
+        train_dataset,
+        shuffle=True,
+        batch_size=config.BATCH_SIZE,
+        num_workers=config.NUM_WORKERS,
+        pin_memory=config.PIN_MEMORY,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        shuffle=True,
+        batch_size=config.BATCH_SIZE,
+        num_workers=config.NUM_WORKERS,
+    )
+    model = model.to(config.DEVICE)
+
+    # Fine tuning the model for 1 epoch
+    scaler = torch.cuda.amp.GradScaler()
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=config.LEARNING_RATE, weight_decay=config.WEIGHT_DECAY)
+
+    if config.LOAD_MODEL and config.CHECKPOINT_FILE in os.listdir():
+        load_checkpoint(torch.load(config.CHECKPOINT_FILE), model)
+
+    for epoch in range(config.DEVICE):
+        train_one_epoch(train_loader, model, loss_fn, optimizer, scaler)
+        check_accuracy(train_loader, model, loss_fn)
+    
+    if config.SAVE_MODEL:
+        checkpoint = {
+            "state_dict": model.state_dict_(),
+            "optimizer": optimizer.state_dict(), 
+        }
+        save_checkpoint(checkpoint, filename=config.CHECKPOINT_FILE)
+    ###
+
+    save_feature_vectors(model, train_loader, output_size=(1, 1), file="train_b7")
+    save_feature_vectors(model, test_loader, output_size=(1, 1), file="test_b7")
+
+
+if __name__ == "__main__":
+    main()
+
+
